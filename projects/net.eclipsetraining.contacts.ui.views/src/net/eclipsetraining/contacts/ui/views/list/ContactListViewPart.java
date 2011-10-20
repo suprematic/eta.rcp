a52c2e08486562d88150391c000fd72fad5caf0d
Ilya Shinkarenko <shinych@gmail.com>
2011-10-20 21:48:23 +0200

    Initial commit: lab 07 - JFace viewers

--- projects/net.eclipsetraining.contacts.ui.views/src/net/eclipsetraining/contacts/ui/views/list/ContactListViewPart.java
+++ projects/net.eclipsetraining.contacts.ui.views/src/net/eclipsetraining/contacts/ui/views/list/ContactListViewPart.java
@@ -1,5 +1,14 @@
 package net.eclipsetraining.contacts.ui.views.list;
 
+import net.eclipsetraining.contacts.core.Contact;
+import net.eclipsetraining.contacts.core.ContactsRepositoryFactory;
+
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
@@ -8,12 +17,16 @@ import org.eclipse.ui.part.ViewPart;
 
 public class ContactListViewPart extends ViewPart {
 
-	private Table tbl;
+	private TableViewer contactsViewer;
 
 	@Override
 	public void createPartControl(Composite parent) {
 
-		tbl = new Table(parent, SWT.NONE);
+		final Composite tableComposite = new Composite(parent, SWT.NONE);
+		final TableColumnLayout tableColumnLayout = new TableColumnLayout();
+		tableComposite.setLayout(tableColumnLayout);
+
+		Table tbl = new Table(tableComposite, SWT.BORDER | SWT.FULL_SELECTION);
 		tbl.setHeaderVisible(true);
 		tbl.setLinesVisible(true);
 
@@ -25,11 +38,47 @@ public class ContactListViewPart extends ViewPart {
 		tcLastName.setText("Last Name");
 		tcLastName.setWidth(150);
 
+		// Init TableViewer
+		contactsViewer = new TableViewer(tbl);
+		contactsViewer.setContentProvider(new ArrayContentProvider());
+		contactsViewer.setComparator(new ContactViewerComparator());
+
+		// First name column
+		final TableViewerColumn tvcFirstName = new TableViewerColumn(contactsViewer, tcFirstName);
+		tableColumnLayout.setColumnData(tvcFirstName.getColumn(), new ColumnWeightData(40));
+		tvcFirstName.setLabelProvider(new ColumnLabelProvider() {
+
+			@Override
+			public String getText(final Object element) {
+				return ((Contact) element).getFirstName();
+			}
+		});
+
+		// Last name column
+		final TableViewerColumn tvcLastName = new TableViewerColumn(contactsViewer, tcLastName);
+		tableColumnLayout.setColumnData(tvcLastName.getColumn(), new ColumnWeightData(60));
+		tvcLastName.setLabelProvider(new ColumnLabelProvider() {
+
+			@Override
+			public String getText(final Object element) {
+				return ((Contact) element).getLastName();
+			}
+		});
+
+		// Finally set input
+		refreshContacts();
+
+	}
+
+	void refreshContacts() {
+
+		Object contacts = ContactsRepositoryFactory.getContactsRepository().getAllContacts();
+
+		contactsViewer.setInput(contacts);
 	}
 
 	@Override
 	public void setFocus() {
-		tbl.setFocus();
+		contactsViewer.getTable().setFocus();
 	}
-
 }
---
+++ projects/net.eclipsetraining.contacts.ui.views/src/net/eclipsetraining/contacts/ui/views/list/ContactViewerComparator.java
@@ -0,0 +1,22 @@
+package net.eclipsetraining.contacts.ui.views.list;
+
+import net.eclipsetraining.contacts.core.Contact;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
+
+class ContactViewerComparator extends ViewerComparator {
+
+	@Override
+	public int compare(final Viewer viewer, final Object e1, final Object e2) {
+
+		if (!(e1 instanceof Contact) || !(e2 instanceof Contact)) {
+			throw new IllegalArgumentException("Can only compare Contacts");
+		}
+
+		final Contact contact1 = (Contact) e1;
+		final Contact contact2 = (Contact) e2;
+
+		return contact1.getLastName().compareTo(contact2.getLastName());
+	}
+}
